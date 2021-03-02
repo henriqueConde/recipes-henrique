13d41b03d37e85eb38e9a772472454b25034778f
Henrique <henriquepenaconde@gmail.com>
2021-03-02 16:23:37 +0000

    food recipes app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '41e17197-d0dd-42a8-9b6d-98eab41a817a';
+export const MODAL_CLOSE_SEC = 2.5;
